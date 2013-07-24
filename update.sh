69c09d9d4227af3080f4c4c3a58d57708023c8fb
Bugamn <bugamn@den>
2013-07-24 20:50:22 -0300

    Add clonning of mercurial repos.

--- update.sh
+++ update.sh
@@ -1,14 +1,21 @@
 root=.vim/bundle
-mercurial_repos=(ropevim)
+
+declare -A mercurial_repos
+mercurial_repos=(
+["ropevim"]="https://bitbucket.org/agr/ropevim"
+)
 
 git submodule foreach git pull
-for repo in ${mercurial_repos[*]}
+for repo in ${!mercurial_repos[@]}
 do
     if [ -e $root/$repo ]
     then
         cd $root/$repo
         hg pull
         cd -
+    else
+        hg clone ${mercurial_repos["$repo"]} $root/$repo
+        echo $repo/ >> .gitignore
     fi
 done
 
